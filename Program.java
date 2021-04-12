bf53d1fa2ca38523c4a0f9484c67475cc8ada2ad
Giuli Ilustre <ilustre.giuli@gmail.com>
2021-04-12 19:20:14 -0300

    Versão 4 - Usando expressão Lambda declarada

--- Program.java
+++ Program.java
@@ -3,6 +3,7 @@ package secao20;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 public class Program {
 
@@ -17,7 +18,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(Product::nonStaticProductPredicate);
+		Predicate<Product> pred = p -> p.getPrice() >= 100.0;
+		
+		list.removeIf(pred);
 		
 		for(Product p : list) {
 			System.out.println(p);
